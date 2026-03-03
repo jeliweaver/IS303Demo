b769457f14cfc9255ff39cb96b9ce66c74ec1269
Charlie-Barnes-03 <charbar4@byu.edu>
2026-03-03 13:17:57 -0700

    Fixed variable names

--- multiply.py
+++ multiply.py
@@ -1,5 +1,5 @@
 # Eli Weaver
 # Description: Function that multiplies two numbers
 
-def multiply_two_numbers(iCharlie1,iMeg2) :
-    return iCharlie1 * iMeg2
+def multiply_two_numbers(iNum1,iNum2) :
+    return iNum1 * iNum2
